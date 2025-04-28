491b8e379065b060ecc89542bbc92c402bc3caa3
zfoudaz <ziyadandali@gmail.com>
2025-04-28 22:17:59 +0300

    change the logic

--- main.py
+++ main.py
@@ -1,86 +1,58 @@
-import streamlit as st
-import io
-from requests import get
-from requests.exceptions import RequestException
-from time import sleep
-import re
+import requests
 import pandas as pd 
+import streamlit as st
+import re
 
-lines=['#EXTM3U\n']
 XTREAM_HOST=st.text_input('Enter the host (ex: http://provider.co:80): ').strip()
-XTREAM_USERNAME=st.text_input('Enter your username: ')
+XTREAM_USERNAME=st.text_input('Enter your username: ').strip()
 XTREAM_PASSWORD=st.text_input('Enter your password: ')
-choice=st.selectbox('Select the type of channels you want to download:', ('Bein Sports , SSC , AD', 'All Channels', 'Sport Channels','custom'))
-custom=''
-df={
+file_name = st.text_input('Enter the file name: ', 'Channels') 
+file_name = file_name if file_name else 'Channels'
+file_data=['#EXTM3u\n']
+filterd_channels = {
     'Channel Name':[],
     'Channel ID':[],
 }
-if choice == 'Bein Sports , SSC , AD':
-    choice = 1
-elif choice == 'All Channels':
-    choice = 2
-elif choice == 'Sport Channels':
-    choice = 3
-elif choice == 'custom':
-    choice = 4
-    custom = st.text_input('Enter the keyword to search for in channel names: ')
-file_name = 'Sports' if choice == 1 or choice == 3 else 'Channels'
-if st.toggle('change file name'):
-    file_name = st.text_input('Enter the file name: ')
+choice = st.selectbox('Select the type of channels you want to download:', ( 'All Channels','Bein Sports , SSC , AD', 'Sport Channels','custom'))
 
-def run():
-    def get_xtream_channels():
-        api_url = f"{XTREAM_HOST}/player_api.php?username={XTREAM_USERNAME}&password={XTREAM_PASSWORD}&action=get_live_streams"
-        
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-        
-        for attempt in range(3):
-            try:
-                response = get(api_url, headers=headers, timeout=10)
-                response.raise_for_status()
-                st.write("Connected to Xtream API successfully.")
-                return response.json()
-            except RequestException as e:
-                st.write(f"Attempt {attempt + 1}/3 : Connection failed - {e} \n make sure you are not using VPN ")
-                sleep(5)
+if choice == 'All Channels': choice = 1
+elif choice == 'Bein Sports , SSC , AD': choice = 2
+elif choice == 'Sport Channels': choice = 3
+elif choice == 'custom': 
+    choice = 4
+    custom_search = st.text_input('Enter the keyword to search for in channel names: ').strip()
+
+def condition(channel , choice):
+    if choice == 1:
+        return True
+    elif choice == 2:
+        return re.search(r'beinsport|bein sport|ssc|ADsport|AD sport',channel['name'],re.IGNORECASE)
+    elif choice == 3:
+        return re.search(r'sport',channel['name'],re.IGNORECASE)
+    elif choice == 4:
+        return re.search(custom_search,channel['name'],re.IGNORECASE)
+
+def get_channels():
+    try: 
+        response = requests.get(f"{XTREAM_HOST}/player_api.php?username={XTREAM_USERNAME}&password={XTREAM_PASSWORD}&action=get_live_streams")
+        return response.json()
+    except:
         return None
 
-    def condition(choice , channel):
-        if choice == 1:
-            return re.search(r'bein|ssc|AD |ADSport', channel['name'],re.IGNORECASE)
-        elif choice==2:
-            return True
-        elif choice==3:
-            return re.search(r'sport', channel['name'], re.IGNORECASE)
-        elif choice==4:
-            return re.search(custom, channel['name'], re.IGNORECASE)
 
-    channels = get_xtream_channels()
-    global df
-    if True:
-        for channel in channels:
-            if condition(choice,channel):
-                lines.append(f"#EXTINF:0,{channel['name']}\n{XTREAM_HOST}/live/{XTREAM_USERNAME}/{XTREAM_PASSWORD}/{channel['stream_id']}.ts\n")
-                df['Channel Name'].append(channel['name'])
-                df['Channel ID'].append(channel['stream_id'])
-
-        buffer = io.BytesIO()
-        for line in lines:
-            buffer.write(line.encode("utf-8"))
-
-        buffer.seek(0)
-        st.success(f'File created successfully with {len(lines)-1} channels')
+if st.button('Get Channels'):
+    all_channels = get_channels()
+    if all_channels:
+        for channel in all_channels:
+            if condition(channel,choice):
+                file_data.append(f"#EXTINF:0,{channel['name']}\n{XTREAM_HOST}/live/{XTREAM_USERNAME}/{XTREAM_PASSWORD}/{channel['stream_id']}.ts\n")
+                filterd_channels['Channel Name'].append(channel['name'])
+                filterd_channels['Channel ID'].append(channel['stream_id'])
+        st.success(f"File created successfully with {len(file_data)-1} channels.")
         st.download_button(
-        label=f"Download {file_name}.m3u",
-        data=buffer,
-        file_name=f"{file_name}.m3u",
-        mime="audio/x-mpegurl"
-        )
-        df=pd.DataFrame(df)
-        st.dataframe(df)
+            label='Download file',
+            data=''.join(file_data),
+            file_name=f"{file_name}.m3u")
+        st.dataframe(pd.DataFrame(filterd_channels,index=range(1,len(filterd_channels['Channel Name'])+1)))
     else:
-        st.error("Failed to retrieve channels. Please check your credentials and try again.")
-
-if st.button('save'):
-    run()
+        st.error("Failed to fetch channels. Please check your credentials and try again.")
